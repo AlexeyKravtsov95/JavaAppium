9874a13efc5dfdb05521bb244bfd84ee208378fa
kravtsov <al.kravtsov@cft.ru>
2021-06-25 12:53:21 +0700

    Перенес файл

--- src/test/java/lib/CoreTestCase.java
+++ src/test/java/lib/CoreTestCase.java
@@ -1,16 +1,9 @@
 package lib;
 
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.android.AndroidDriver;
 import junit.framework.TestCase;
-import lib.ui.Platform;
-import org.junit.After;
-import org.junit.Before;
 import org.openqa.selenium.ScreenOrientation;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
-import java.net.URL;
 import java.time.Duration;
 
 public class CoreTestCase extends TestCase {
--- src/test/java/lib/Platform.java
+++ src/test/java/lib/Platform.java
@@ -1,4 +1,4 @@
-package lib.ui;
+package lib;
 
 import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
